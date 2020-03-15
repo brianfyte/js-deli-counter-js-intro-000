bf72b13558381618b72388b7a783313a74781a28
Brian Wong <brianw385@gmail.com>
2020-03-15 15:14:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ function takeANumber(katzDeli, name) {
   return "Welcome, " + `${name}` + ". You are number " + `${katzDeli.length}` + " in line."
 }
 
-var katzDeliLine = []
+
 function nowServing(katzDeliLine) {
-  var i = 0;
-  while (i <= katzDeliLine.length) {
-    if (katzDeliLine.length !== 0) {
+  if (katzDeliLine.length !== 0) {
     return "Currently serving " + `${katzDeliLine.shift()}` + ".";
-    }
-    else return "There is nobody waiting to be served!";
+  }
+  else {
+    return "There is nobody waiting to be served!";
   }
 }
 
+
 function currentLine(line) {
-  var i = 0
-  while (i < line.length) {
-    katzDeliLine.push(" " + [i + 1] + ". " + line[i])
-    i++;
-  }
+  var katzDeliLine = []
+  
   if (line.length === 0){
     return "The line is currently empty."
   } else {
-    return "The line is currently:" + katzDeliLine;
+      var i = 0
+      while (i < line.length) {
+        katzDeliLine.push(" " + [i + 1] + ". " + line[i])
+        i++;
+      }
+      return "The line is currently:" + katzDeliLine;
   }
 }
